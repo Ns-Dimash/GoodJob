84ca594163f3ad78f73e84fc3239646648b07919
Dimash Nsanbaev <nsanbaev.15@gmail.com>
2023-09-07 19:30:28 +0600

    First

---
+++ GoodJob/GoodJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodJobApp.swift
+//  GoodJob
+//
+//  Created by Dimash Nsanbaev on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
