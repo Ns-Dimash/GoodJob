fd48ebf601b3901557524f4ec85e7baf3790858b
Rystem Asqar <bornqazaqi@gmail.com>
2023-09-07 19:32:57 +0600

    Test committing

---
+++ GoodJob/IntentView.swift
@@ -0,0 +1,20 @@
+//
+//  IntentView.swift
+//  GoodJob
+//
+//  Created by Rystem Asqar on 9/7/23.
+//
+
+import SwiftUI
+
+struct IntentView: View {
+    var body: some View {
+        Text("Hello, I am Dimash")
+    }
+}
+
+struct IntentView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntentView()
+    }
+}
